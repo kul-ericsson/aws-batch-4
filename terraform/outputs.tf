f9b3221a633860177e27305d00e07b1c319e9bd5
Kulbhushan <kulbhushan.mayer@>
2020-12-14 12:52:45 +0530

    moved outputs to separate file

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "thinknyx_ami" {
+  value = data.aws_ami.thinknyx_ami.id
+}
+
+output "thinknyx_ec2_server_public_ip" {
+  value = aws_instance.thinknyx_ec2_server.public_ip
+}
+
+output "thinknyx_vpc" {
+  value = data.aws_vpc.thinknyx_vpc.id
+}
