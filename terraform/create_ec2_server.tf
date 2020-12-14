fbeac76ffe41ab825820a457d21ae4b0b2688f6f
Kulbhushan <kulbhushan.mayer@>
2020-12-14 11:52:48 +0530

    added code to create EC2 Server

--- terraform/create_ec2_server.tf
+++ terraform/create_ec2_server.tf
@@ -6,6 +6,19 @@ data "aws_ami" "thinknyx_ami" {
   }
 }
 
+resource "aws_instance" "thinknyx_ec2_server" {
+  ami = data.aws_ami.thinknyx_ami.id
+  instance_type = "t2.micro"
+  key_name = "ericsson-batch-4-kul"
+  tags = {
+    "Name" = "thinknyx_ec2_server"
+  }
+}
+
 output "thinknyx_ami" {
   value = data.aws_ami.thinknyx_ami.id
+}
+
+output "thinknyx_ec2_server_public_ip" {
+  value = aws_instance.thinknyx_ec2_server.public_ip
 }
