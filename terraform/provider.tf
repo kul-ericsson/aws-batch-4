24e9e5b42872f46bc60edafd39032d7942b9edf3
Kulbhushan <kulbhushan.mayer@>
2020-12-14 16:41:51 +0530

    added new provider for mysql

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,9 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+provider "mysql" {
+  endpoint = aws_db_instance.thinknyx_mysql_db.endpoint
+  username = aws_db_instance.thinknyx_mysql_db.username
+  password = aws_db_instance.thinknyx_mysql_db.password
+}
