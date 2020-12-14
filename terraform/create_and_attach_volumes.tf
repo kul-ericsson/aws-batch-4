a9b4a151da05ba87f868a0c611e5f46073222c7f
Kulbhushan <kulbhushan.mayer@>
2020-12-14 13:53:33 +0530

    updated to attach valume to server

--- terraform/create_and_attach_volumes.tf
+++ terraform/create_and_attach_volumes.tf
@@ -5,3 +5,10 @@ resource "aws_ebs_volume" "thinknyx_addtional_volume" {
       Name = "thinknyx_ec2_server_public"
   }
 }
+
+resource "aws_volume_attachment" "thinknyx_attach_volume" {
+  volume_id = aws_ebs_volume.thinknyx_addtional_volume.id
+  instance_id = aws_instance.thinknyx_ec2_server_public.id
+  device_name = "/dev/sdb"
+  skip_destroy = true
+}
