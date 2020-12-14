0f0be5153644281915db6d5234373059d9942c8e
Kulbhushan <kulbhushan.mayer@>
2020-12-14 11:03:08 +0530

    added code to ceate Internet Gateway

--- terraform/configure_vpc.tf
+++ terraform/configure_vpc.tf
@@ -17,3 +17,10 @@ resource "aws_subnet" "thinknyx_subnet_public" {
   }
   availability_zone = "us-east-2a"
 }
+
+resource "aws_internet_gateway" "thinknyx_internet_gateway" {
+  vpc_id = aws_vpc.thinknyx_vpc.id
+  tags = {
+    "Name" = "thinknyx_internet_gateway"
+  }
+}
