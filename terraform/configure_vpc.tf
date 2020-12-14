1fe997580c12b2894eb4040fb8b3366fbd81ed18
Kulbhushan <kulbhushan.mayer@>
2020-12-14 11:03:22 +0530

    added code to ceate Route Table

--- terraform/configure_vpc.tf
+++ terraform/configure_vpc.tf
@@ -24,3 +24,14 @@ resource "aws_internet_gateway" "thinknyx_internet_gateway" {
     "Name" = "thinknyx_internet_gateway"
   }
 }
+
+resource "aws_route_table" "thinknyx_route_table_public" {
+  vpc_id = aws_vpc.thinknyx_vpc.id
+  tags = {
+    "Name" = "thinknyx_route_table_public"
+  }
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.thinknyx_internet_gateway.id
+  }
+}
